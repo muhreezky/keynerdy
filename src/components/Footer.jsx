8bd42b5ce33bcbcce834743b7d632cb96e233f6f
muhreezky <muhammadrizkiansyah60@gmail.com>
2023-03-09 10:55:36 +0700

    Added footer in Help.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Footer as Foot } from "react-daisyui";
+import { FaInstagram, CgWebsite } from "react-icons/all";
 
 function Footer() {
   return (
-    <Foot className="p-20">
-      &copy; MuhReezky Studio {(new Date()).getFullYear()}
+    <Foot className="p-20 text-xl">
+      &copy; MuhReezky Studio {(new Date()).getFullYear()}<br />
+      <a href="https://muhreezky.my.id" className="flex" target="_blank">
+        <FaInstagram />
+        @muh.reezky
+      </a><br />
+      <a href="https://muhreezky.my.id" className="flex" target="_blank">
+        <CgWebsite />
+        https://muhreezky.my.id
+      </a>
     </Foot>
   )
 }
--- src/components/Help.jsx
+++ src/components/Help.jsx
@@ -1,41 +1,45 @@
 import { useEffect } from 'react';
 import { Card, Button } from "react-daisyui";
 import BackButton from './BackButton';
+import Footer from './Footer';
 
 function Help() {
   useEffect(() => {
     document.title = "KeyNerdy Help Page";
   }, []);
   return (
-    <Card className="text-black bg-slate-50 dark:bg-slate-700 dark:text-white w-[90%] shadow-xl dark:shadow-blue-800">
-      <Card.Body>
-        <Card.Title className="mb-3">
-          <h1 className="float-left text-3xl">
-            <b>KeyNerdy Help Page</b>
-          </h1>
-        </Card.Title>
-        <div className="p-3">
-          <h2 className="text-xl">How to Play</h2>
-          <ol className="list-decimal">
-            <li>
-              Click Play Button
-            </li>
-            <li>
-              You have 1 minute to play a round
-            </li>
-            <li>
-              Type as fast as you can to get score
-            </li>
-            <li>
-              If the time ran out, your typing speed will automatically calculated
-            </li>
-          </ol>
-        </div>
-      </Card.Body>
-      <Card.Actions className="p-8">
-        <BackButton />
-      </Card.Actions>
-    </Card>
+    <div p="5">
+      <Card className="text-black bg-slate-50 dark:bg-slate-700 dark:text-white w-full shadow-xl dark:shadow-blue-800">
+        <Card.Body>
+          <Card.Title className="mb-3">
+            <h1 className="float-left text-3xl">
+              <b>KeyNerdy Help Page</b>
+            </h1>
+          </Card.Title>
+          <div className="p-3">
+            <h2 className="text-xl">How to Play</h2>
+            <ol className="list-decimal">
+              <li>
+                Click Play Button
+              </li>
+              <li>
+                You have 1 minute to play a round
+              </li>
+              <li>
+                Type as fast as you can to get score
+              </li>
+              <li>
+                If the time ran out, your typing speed will automatically calculated
+              </li>
+            </ol>
+          </div>
+        </Card.Body>
+        <Card.Actions className="p-8">
+          <BackButton />
+        </Card.Actions>
+      </Card><br />
+      <Footer />
+    </div>
   )
 }
 
