b9b5331a05a8f9a7dc9ce680dc1266011d085aed
muhreezky <muhammadrizkiansyah60@gmail.com>
2023-03-10 11:25:11 +0700

    Committed Again

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createMemoryRouter, RouterProvider } from "react-router-dom";
+
+// Anda dapat sesuka hati mengganti createMemoryRouter menjadi BrowserRouter ataupun HashRouter tergantung kebutuhan anda
+import { createMemoryRouter as createRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
 
-const myroutes = createMemoryRouter(routes);
+// Setting routes URL
+const myroutes = createRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
